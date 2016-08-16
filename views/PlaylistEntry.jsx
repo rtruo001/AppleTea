10f9e9ee02ad5109799a4020cd5baa2650374179
Gerard Liu <liu.gerard@gmail.com>
2016-08-16 12:47:41 -0700

    Added demo of MyPlaylists, created PlaylistEntry.jsx, renamed PrivatePlaylists.

---
+++ views/PlaylistEntry.jsx
@@ -0,0 +1,70 @@
+var React = require('react');
+
+// Icon displayed depends on whether playlist is public, private, or not owner
+var PlaylistIcon = React.createClass({
+  render: function () {
+    if (this.props.owner == false) {
+      return (
+        <div className="playlist-icon">
+          <a className="icon-btn like-btn" href="javascript:void(0)">
+            <i className="fa fa-heart-o" aria-hidden="true"></i>
+          </a>
+        </div>
+      );
+    }
+    else if (this.props.owner == true && this.props.type == 'private') {
+      return (
+        <div className="playlist-icon">
+          <i className="fa fa-lock" aria-hidden="true"></i>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="playlist-icon">
+          <i className="fa fa-globe" aria-hidden="true"></i>
+        </div>
+      );
+    };
+  }
+});
+
+// MAIN COMPONENT: Each individual playlist card
+var PlaylistEntry = React.createClass({
+  render: function() {
+
+    // If owner, append user-playlist to classname
+    var playlistCardClassName = "playlist-card";
+    if (this.props.owner == true) {
+      playlistCardClassName += " user-playlist";
+    };
+
+    return (
+      <div className="col-md-3 col-sm-4 col-padding">
+          <div className={playlistCardClassName}>
+            <div className="playlist-img-container">
+              <a href="javascript:void(0)">
+                <div className="playlist-overlay">
+                  <div className="playlist-play-icon"><i className="fa fa-play" aria-hidden="true"></i></div>
+                  PLAY ALL
+                </div>
+              </a>
+              <img className="playlist-img" src="images/media-icon.png" />
+            </div>
+            <div className="playlist-text-container">
+              <div className="playlist-title">
+                <a className="playlist-link playlist-title-text ellipses" data-toggle="tab" href="#open-playlist">{this.props.title}</a>
+                <div className="pill" data-toggle="tooltip" title="Items in Playlist">{this.props.size}</div>
+              </div>
+              <div className="playlist-curator"><a className="curator-link" data-toggle="tab" href="#curator-page">{this.props.curator}</a></div>
+            </div>
+            <div className="playlist-icon-container">
+              <PlaylistIcon owner={this.props.owner} type={this.props.type} liked={this.props.liked}/>
+            </div>
+          </div>
+      </div>
+    );
+  }
+});
+
+module.exports = PlaylistEntry;
