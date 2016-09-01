15599ae35e3beddc6c5225a5264a90cd4a510356
Gerard Liu <liu.gerard@gmail.com>
2016-08-31 17:37:58 -0700

    created 'Add New Playlist' button and modal popup to create new playlist.

--- views/MyPlaylists.jsx
+++ views/MyPlaylists.jsx
@@ -30,6 +30,53 @@ var SearchMyPlaylist = React.createClass({
   }
 });
 
+// Create New Playlist Button
+var NewPlaylistButton = React.createClass({
+  render: function() {
+    return (
+      <div className="search-container btn-align-right">
+        <button className="btn btn-primary" data-toggle="modal" data-target="#create-playlist"><i className="fa fa-plus icon-padding"></i>Create New Playlist</button>
+      </div>
+    );
+  }
+});
+
+// Create New Playlist Modal Popup
+var NewPlaylistModal = React.createClass({
+  render: function() {
+    return (
+      <div className="modal fade" id="create-playlist" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className="modal-dialog modal-sm" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              Create a New Playlist
+            </div>
+            <div className="modal-body">
+              <div className="search-container">
+                <form className="search-input" id="create-playlist-input">
+                  <input className="input-padding" type="text" placeholder="Playlist Name"/>
+                  <div className="modal-label">Is this a private playlist?</div>
+                  <div className="toggle-slider-section">
+                    <label className="switch">
+                      <input type="checkbox" id="create-room-toggle" checked />
+                      <div className="slider"></div>
+                    </label>
+                    <i className="fa fa-lock" id="create-room-toggle-icon"></i>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <button type="button" className="btn btn-primary" data-dismiss="modal">Create Playlist</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
 // MAIN COMPONENT: My Playlist Tab
 var MyPlaylists = React.createClass({
   render: function() {
@@ -88,6 +135,8 @@ var MyPlaylists = React.createClass({
 
     return (
       <div>
+        <NewPlaylistButton />
+        <NewPlaylistModal />
         {playlistEntries}
       </div>
     );
